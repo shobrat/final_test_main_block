722f40ec9011e35a4326934fb5ede394e4307948
Xaknazarov Sharasul <741922@bk.ru>
2024-03-04 17:43:37 +0500

    add folder function_dz2

---
+++ function_dz2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите целые числа");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+int sum = 0;
+int count = num.ToString().Length;
+for (int i = 0; i < count; i++)
+{
+    int del = num % 10;
+    num = num / 10;
+    sum = sum + del;
+}
+
+Console.WriteLine($"{num} {sum}");
