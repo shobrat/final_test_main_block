10de579706ededb63181872182ea2e90f8a393b9
Xaknazarov Sharasul <741922@bk.ru>
2024-03-04 18:02:31 +0500

    add changes function_dz2/Program.cs

--- function_dz1/Program.cs
+++ function_dz1/Program.cs
@@ -1,27 +1,115 @@
 ﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
 // Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+// 5 12 16 q [STOP] 3 45 342 15 [STOP]
 
-Console.WriteLine("Введите целые числа");
-int num = Convert.ToInt32(Console.ReadLine());
+int size = 100;
+int[] arr_int = new int[size];
+int i = 0;
+int sum = i / 10 + i % 10;
 
-
-
-static int FindSumNumbers(int num)
+while (i < size)
 {
-    int sum = 0;
-    int count = num.ToString().Length;
-    for (int i = 0; i < count; i++)
-    {
-        int del = num % 10;
-        num /= 10;
-        sum += del;
-    }
-    return sum;
+    Console.WriteLine("Введите целое число, если хотите завершить: введите q или число, сумма которого четная");
+    string input = Console.ReadLine();
+    arr_int[i] = Convert.ToInt32(input);
+    i = i + 1;
+
+    string q = Console.ReadLine();
+
+    if (q == "stop") break;
+
+    if (sum % 2 == 0) Console.WriteLine($"{sum}"); break;
 }
 
-int result = FindSumNumbers(num);
 
-Console.WriteLine($"{result}");
+
+// да в первой задаче надо проверять на
+// а) что бы в string не попало нулабельное значение
+// 6) что бы был выход, когда будет "q"
+// в) если не попадает в первые два случая, пробуем конвертировать в число, если получается, то считаем сумму цифр и проверяем на четность
+// г) если не получается, значит там строка, просто выводим ее в консоль
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Console.WriteLine("Введите целые числа");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+// static int CountSumNumbers(int num)
+// {
+//     int sum = 0;
+//     int count = num.ToString().Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         int del = num % 10;
+//         num /= 10;
+//         sum += del;
+//     }
+//     return sum;
+// }
+
+// int[] BreakEnterNumbers(int[] array)
+// {
+//     i=0;
+//     while(true)
+//     {
+//         i++;
+//         Console.WriteLine(i);
+//     }
+// }
+
+// BreakEnterNumbers(num);
+
+// int result = CountSumNumbers(num);
+
+// Console.WriteLine($"{result}");
+
+
+// static void Main(string[] args)
+//         {
+//             int i = 0;
+//             for (; ; )
+//             {
+//                 i++;
+//                 Console.WriteLine(i);
+//                 if (i==12345) break;
+//             }
+//             Console.ReadLine();
+//         }
+
 
 
 // int BreakEnterNumbers(int num)
--- function_dz2/Program.cs
+++ function_dz2/Program.cs
@@ -1,15 +1,60 @@
-﻿
-Console.WriteLine("Введите целые числа");
-int num = Convert.ToInt32(Console.ReadLine());
+﻿// Задайте массив заполненный случайными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [344 452 341 125] => 2
 
-
-int sum = 0;
-int count = num.ToString().Length;
-for (int i = 0; i < count; i++)
+int[] CreateArrayRndInt(int size, int min, int max)
 {
-    int del = num % 10;
-    num = num / 10;
-    sum = sum + del;
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+    return array;
 }
 
-Console.WriteLine($"{num} {sum}");
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+int FindEvenNumbers(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            sum += 1;
+        }
+    }
+    return sum;
+}
+
+int minimum = 100;
+int maximum = 1000;
+int count = 25;
+
+int[] array = CreateArrayRndInt(count, minimum, maximum);
+PrintArray(array);
+
+Console.WriteLine($" => {FindEvenNumbers(array)}");
+
+// Console.WriteLine("Введите целые числа");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+
+// int sum = 0;
+// int count = num.ToString().Length;
+// for (int i = 0; i < count; i++)
+// {
+//     int del = num % 10;
+//     num = num / 10;
+//     sum = sum + del;
+// }
+
+// Console.WriteLine($"{num} {sum}");
