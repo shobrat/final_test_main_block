566ae92f4c49ff73b866dc6d725ac84cbaedd197
Xaknazarov Sharasul <741922@bk.ru>
2024-03-04 17:44:10 +0500

    add changes

--- function_dz1/Program.cs
+++ function_dz1/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая бесконечно запрашивает целые числа с консоли. 
+// Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+Console.WriteLine("Введите целые числа");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+
+static int FindSumNumbers(int num)
+{
+    int sum = 0;
+    int count = num.ToString().Length;
+    for (int i = 0; i < count; i++)
+    {
+        int del = num % 10;
+        num /= 10;
+        sum += del;
+    }
+    return sum;
+}
+
+int result = FindSumNumbers(num);
+
+Console.WriteLine($"{result}");
+
+
+// int BreakEnterNumbers(int num)
+// {
+//     int number=0;
+// }
