779ac7d7106e9de7a00f2f4108a0d2437b05fe4d
Sharasul <sh.haqnazarov@bipartners.uz>
2024-03-10 15:12:07 +0500

    modified:function_dz1/Program.cs

--- function_dz1/Program.cs
+++ function_dz1/Program.cs
@@ -2,51 +2,120 @@
 // Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
 // 5 12 16 q [STOP] 3 45 342 15 [STOP]
 
-
 using System;
-
 class Program
 {
-
-    // public string EqualsQ()
-    // {
-    //     string q = "q";
-    //     return q;
-    // }
-    public static int Main()
+    static void Main()
     {
-        /*переменная, которой потом присвоим значение этой суммы, 
-        должна сразу быть инициализирована нулем*/
-        double res = 0;
-        //string qu = "q";
-        int stringToInt = Convert.ToInt32("q");
-        for (; ; )
+        while (true) // Бесконечный цикл
         {
-            Console.Write("Введите число, для выхода введите 0 : ");
-            double num = double.Parse(Console.ReadLine());
-            //int num = Convert.ToInt32(Console.ReadLine());
-            // string? num = Console.ReadLine();
-
-            if (num == 0)
+            Console.Write("Введите число или 'q' для выхода: ");
+            string input = Console.ReadLine(); // Чтение строки ввода пользователя
+            if (input == "q") // Проверка на ввод 'q' для выхода
             {
                 break;
             }
-            res += num;
-            
-            if (num == stringToInt)
+            int number;
+            if (int.TryParse(input, out number)) // Проверка, является ли ввод числом Метод int.TryParse используется для попытки 
+            //преобразования строки в целое число типа int. Первый аргумент input представляет собой строку для преобразования, 
+            //а второй аргумент out int number - это переменная, в которую будет сохранено преобразованное значение.
+            //- out: Ключевое слово out указывает, что аргумент number передается по ссылке, а не по значению. 
+            //Это означает, что метод int.TryParse может изменить значение переменной number внутри себя 
+            //(если преобразование произошло успешно), и эти изменения будут видны в основной функции.й функции.
+            //Иными словами, после выполнения метода int.TryParse, переменная number будет содержать преобразованное значение 
+            //из строки input в целое число типа int,
             {
-                break;
+                int sum = 0;
+                while (number > 0) // Вычисление суммы цифр числа
+                {
+                    sum += number % 10; // Добавление последней цифры к сумме
+                    number /= 10; // Удаление последней цифры из числа
+                }
+                if (sum % 2 == 0) // Проверка, является ли сумма цифр четной
+                {
+                    Console.WriteLine("[STOP]");
+                    break;
+                }
             }
-            
 
+            else // Если ввод не является числом и не 'q', повторить запрос
+            {
+                Console.WriteLine("Некорректный ввод. Пожалуйста, введите целое число или 'q'.");
+            }
         }
-        Console.WriteLine("\nСумма : {0}", res);
-        Console.ReadKey();
-        return 0;
     }
-
-
 }
+
+
+// using System;
+
+// class Program
+// {
+//     static void Main()
+//     {
+//         while (true)
+//         {
+//             Console.WriteLine("Введите целое число или 'q' для выхода:");
+//             string input = Console.ReadLine();
+
+//             if (input.ToLower() == "q")
+//             {
+//                 Console.WriteLine("Программа завершена.");
+//                 break;
+//             }
+
+//             if (int.TryParse(input, out int number))
+//             {
+//                 int sum = CalculateDigitSum(Math.Abs(number));
+
+//                 if (sum % 2 == 0)
+//                 {
+//                     Console.WriteLine($"Сумма цифр числа {number} четная. Программа завершена.");
+//                     break;
+//                 }
+//             }
+//         }
+//     }
+
+//     // Метод для вычисления суммы цифр числа
+//     static int CalculateDigitSum(int num)
+//     {
+//         int sum = 0;
+//         while (num != 0)
+//         {
+//             sum += num % 10;
+//             num /= 10;
+//         }
+//         return sum;
+//     }
+// }
+
+
+// /*переменная, которой потом присвоим значение этой суммы, 
+// должна сразу быть инициализирована нулем*/
+// double res = 0;
+// //string qu = "q";
+// int stringToInt = Convert.ToInt32("q");
+// for (; ; )
+// {
+//     Console.Write("Введите число, для выхода введите 0 : ");
+//     double num = double.Parse(Console.ReadLine());
+//     //int num = Convert.ToInt32(Console.ReadLine());
+//     // string? num = Console.ReadLine();
+//     if (num == 0)
+//     {
+//         break;
+//     }
+//     res += num;
+
+//     if (num == stringToInt)
+//     {
+//         break;
+//     }
+// }
+// Console.WriteLine("\nСумма : {0}", res);
+// Console.ReadKey();
+// return 0;
 
 // Console.WriteLine("Введите целое число, если хотите завершить: введите q или число, сумма которого четная");
 // int num = Convert.ToInt32(Console.ReadLine());
