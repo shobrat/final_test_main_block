c0ebfd59a482c9a2fac5908b7ccb26e09dfb2a92
Sharasul <sh.haqnazarov@bipartners.uz>
2024-07-14 16:44:14 +0500

    add new file

---
+++ dz_block1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class FindStrings
+{
+    public static void Main(string[] args)
+    {
+        // Ввод строк
+        string[] Array = { "Hello", "2", "world", ":-)" };
+        
+        // размер нового массива
+        int count = 0;
+        foreach (string str in Array)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        // новый массив
+        string[] newArray = new string[count];
+        int index = 0;
+
+        // Заполнение
+        foreach (string str in Array)
+        {
+            if (str.Length <= 3)
+            {
+                newArray[index] = str;
+                index++;
+            }
+        }
+
+        // Вывод
+        Console.Write("[");
+        for (int i = 0; i < newArray.Length; i++)
+        {
+            Console.Write(newArray[i]);
+            if (i < newArray.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
+    }
+}
---
+++ dz_block2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+public class FindStrings
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Введите строки через пробелы:");
+        string input = Console.ReadLine();
+        string[] Array = input.Split(" ");
+
+        int count = 0;
+        foreach (string str in Array)
+        {
+            if (str.Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        // Новый массив
+        string[] newArray = new string[count];
+        int index = 0;
+
+        // Заполнение
+        foreach (string str in Array)
+        {
+            if (str.Length <= 3)
+            {
+                newArray[index] = str;
+                index++;
+            }
+        }
+
+        // вывод
+        Console.Write("[");
+        for (int i = 0; i < newArray.Length; i++)
+        {
+            Console.Write(newArray[i]);
+            if (i < newArray.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
+    }
+}
